823828e1f8742e549a7629fbd82a23a132f0304e
Juan <jtrj33@gmail.com>
2017-09-07 16:49:49 -0500

    Updated unit tests in TestMatrix.cpp

--- Matrix/TestMatrix.cpp
+++ Matrix/TestMatrix.cpp
@@ -10,8 +10,29 @@
 #include <gtest/gtest.h>
 #include "Matrix.hpp"
 
-TEST(test_matrix, test_getters)
+TEST(test_matrix, test_getters_1)
 {
     Matrix<int> A(1, 3, 1);
     ASSERT_EQ(A.getRows(), 1);
+    ASSERT_EQ(A.getCols(), 3);
+}
+
+TEST(test_matrix, test_getters_2)
+{
+    Matrix<int> A = Matrix<int>();
+    ASSERT_EQ(A.getRows(), 1);
+    ASSERT_EQ(A.getCols(), 1);
+}
+
+TEST(test_matrix, test_index_operator_1)
+{
+    Matrix<int> A(3, 3);
+    ASSERT_EQ(A.at(1, 1), 0);
+    ASSERT_EQ(A(1,1), 0);
+}
+
+TEST(test_matrix, test_index_operator_2)
+{
+    Matrix<int> A(1,1);
+    ASSERT_THROW(A.at(2, 2), std::out_of_range);
 }
